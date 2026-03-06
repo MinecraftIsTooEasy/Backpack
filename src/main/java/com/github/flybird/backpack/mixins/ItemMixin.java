daede70f4a5b81b8b767ee1d2b53a7cb93d191dc
Inf1nlty <60602644+Inf1nlty@users.noreply.github.com>
2026-03-07 00:57:38 +0800

    Fix

--- src/main/java/com/github/flybird/backpack/mixins/ItemMixin.java
+++ src/main/java/com/github/flybird/backpack/mixins/ItemMixin.java
@@ -1,7 +1,7 @@
 package com.github.flybird.backpack.mixins;
 
+import com.github.flybird.backpack.BPRegistryInit;
 import net.minecraft.*;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,9 +12,13 @@ import java.util.List;
 @Mixin(Item.class)
 public class ItemMixin {
 
-    @SuppressWarnings("unchecked, rawtypes")
+    @SuppressWarnings({"unchecked", "rawtypes"})
     @Inject(method = "addInformationBeforeEnchantments", at = @At("HEAD"))
-    private void addInformationBeforeEnchantments(ItemStack item_stack, EntityPlayer player, List info, boolean extended_info, Slot slot, CallbackInfo ci) {
-        info.add(EnumChatFormatting.YELLOW + I18n.getString("item.backpack.single_hint"));
+    private void addInformationBeforeEnchantments(ItemStack item_stack, EntityPlayer player, List info, boolean extended_info, Slot slot, CallbackInfo ci)
+    {
+        if (item_stack.getItem() == Item.getItem(BPRegistryInit.backpack))
+        {
+            info.add(EnumChatFormatting.YELLOW + I18n.getString("item.backpack.single_hint"));
+        }
     }
 }
